53169970c36adc8c26b8160d8d84c9b492730774
Pauline <154602894+Philopolis@users.noreply.github.com>
2024-02-16 00:49:32 +0800

    Add files via upload

--- LaunchVehicleTrajectory/Code/Propulsion.m
+++ LaunchVehicleTrajectory/Code/Propulsion.m
@@ -3,16 +3,12 @@ function P_0 = Propulsion(t)
 
 if t > 0 && t < 61.6
     P_0 = 912000;
+elseif t > 61.6 && t < 126.8
+    P_0 = 270000;
+elseif t > 126.8 && t < 186.4
+    P_0 = 155000;
 else
-    if t > 61.6 && t < 126.8
-        P_0 = 270000;
-    else
-        if t > 126.8 && t < 186.4
-            P_0 = 155000;
-        else
-            P_0 = 0;
-        end
-    end
+    P_0 = 0;
 end
 end
 
--- LaunchVehicleTrajectory/Code/varphi_program.m
+++ LaunchVehicleTrajectory/Code/varphi_program.m
@@ -9,12 +9,10 @@ fig = pi/60;
 
 if t < t1
     varphi_pr = pi/2;
+elseif t < t2
+    varphi_pr = pi/2 + (pi/2 - fig) * ((t-t1)^2/((t2-t1).^2) - 2 * (t-t1)/(t2-t1));
 else
-    if t < t2
-        varphi_pr = pi/2 + (pi/2 - fig) * ((t-t1)^2/((t2-t1).^2) - 2 * (t-t1)/(t2-t1));
-    else
-        varphi_pr = fig;
-    end
+    varphi_pr = fig;
 end
 end
 
