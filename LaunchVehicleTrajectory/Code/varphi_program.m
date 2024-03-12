29df9367d9153ad2e3ddbe0a3952adf0da964850
Pauline <154602894+Philopolis@users.noreply.github.com>
2024-03-12 17:57:07 +0800

    update

--- LaunchVehicleTrajectory/Code/varphi_program.m
+++ LaunchVehicleTrajectory/Code/varphi_program.m
@@ -1,7 +1,7 @@
-%% ³ÌĞò½Çº¯Êı
+%% ç¨‹åºè§’å‡½æ•°
 function varphi_pr = varphi_program(t)
 
-%³ÌĞò½Ç²ÎÊı
+% ç¨‹åºè§’å‚æ•°
 t1 = 10;
 t2 = 160;
 %t3 = 186.4;
@@ -10,7 +10,7 @@ fig = pi/60;
 if t < t1
     varphi_pr = pi/2;
 elseif t < t2
-    varphi_pr = pi/2 + (pi/2 - fig) * ((t-t1)^2/((t2-t1).^2) - 2 * (t-t1)/(t2-t1));
+    varphi_pr = pi/2 + (pi/2 - fig) * ((t-t1)^2/((t2-t1)^2) - 2 * (t-t1)/(t2-t1));
 else
     varphi_pr = fig;
 end
